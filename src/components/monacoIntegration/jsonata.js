47a38d5ba1f76d1223181ef43687ebb52eef1de5
martinholden-skillsoft <12408585+martinholden-skillsoft@users.noreply.github.com>
2020-05-20 12:43:42 +0100

    Added start of completionItems

--- src/components/monacoIntegration/jsonata.js
+++ src/components/monacoIntegration/jsonata.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-template-curly-in-string */
 import monarchDefinition from './jsonata.monarch';
 
 export default function (monaco) {
@@ -42,5 +43,53 @@ export default function (monaco) {
       { token: 'jsonata-variable', foreground: 'ff4000' },
       { token: 'jsonata-names', foreground: '0000c0' },
     ],
+  });
+
+  // Register a completion item provider for the new language
+  monaco.languages.registerCompletionItemProvider('jsonata', {
+    provideCompletionItems: () => {
+      var suggestions = [
+        {
+          label: '$successFactorsType',
+          kind: monaco.languages.CompletionItemKind.Snippet,
+          insertText: 'successFactorsType(${1:types}, ${2:row})',
+          insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+          detail:
+            'Return string that represents the CPNT_TYP_ID and DEL_MTH_ID type id string in SuccessFactors',
+          documentation:
+            'types - Configuration object that defines the type configurations, typically from $types in base Configuration.\nrow - Object that represents the record, typically $.',
+        },
+        {
+          label: '$successFactorsCompletionStat',
+          kind: monaco.languages.CompletionItemKind.Snippet,
+          insertText: 'successFactorsCompletionStat(${1:types}, ${2:row})',
+          insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+          detail:
+            'Return string that represents the CMPL_STAT_ID completion rule string in SuccessFactors',
+          documentation:
+            'types - Configuration object that defines the type configurations, typically from $types in base Configuration.\nrow - Object that represents the record, typically $.',
+        },
+        {
+          label: '$successFactorsShowInCatalog',
+          kind: monaco.languages.CompletionItemKind.Snippet,
+          insertText: 'successFactorsShowInCatalog(${1:types}, ${2:row})',
+          insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+          detail: 'Return "Y" or "N" indicating if this asset should show in the catalog.',
+          documentation:
+            'types - Configuration object that defines the type configurations, typically from $types in base Configuration.\nrow - Object that represents the record, typically $.',
+        },
+        {
+          label: '$successFactorsLocale',
+          kind: monaco.languages.CompletionItemKind.Snippet,
+          insertText: 'successFactorsLocale(${1:localeCode})',
+          insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+          detail:
+            'Return empty string if the argument was null or undefined.  Otherwise, it uses a library to lookup the one word name of the language.  For example, en-US would return "English".  fr-CA would return "French',
+          documentation:
+            'localeCode - This argument is a nullable string that represents the locale code, typically from $.localizedMetadata[0].localeCode',
+        },
+      ];
+      return { suggestions: suggestions };
+    },
   });
 }
