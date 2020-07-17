fd872fd07fa319922ef8bd9e664b904b50f43c81
martinholden-skillsoft <12408585+martinholden-skillsoft@users.noreply.github.com>
2020-07-17 10:10:51 +0100

    Update signatures

--- src/components/monacoIntegration/functions/generic.js
+++ src/components/monacoIntegration/functions/generic.js
@@ -1,5 +1,51 @@
 export default [
   {
+    label: 'shortenUuid',
+    description: 'Shorten the supplied UUID string, by converting to basen string',
+    returns: 'String',
+    parameters: [
+      {
+        label: 'uuid',
+        type: 'String',
+        optional: false,
+        default: null,
+        documentation: 'The uuid string to shorten',
+      },
+      {
+        label: 'encoding',
+        type: 'String',
+        optional: true,
+        default: 'base62',
+        documentation:
+          'A string with two possible values.\n\n * base64url\n\n * base64\n\n * base62\n\n * base58\n\n * base36\n\n * base32\n\n * base16\n\n * base10\n\n * base2\n\n',
+      },
+    ],
+    signature: '(uuid,encoding)',
+  },
+  {
+    label: 'extendedTitle',
+    description:
+      'Get the extended title.\n\n  * If title already includes a colon no nothing\n  * If we have a **$.technologies**, preface the title with the **$.technologies[0].title and $.technologies[0].version**\n  * If we have a **$.technologies[0].title** and no **$.technologies[0].version** only preface with **$.technologies[0].title**\n  * If we have a **$.technologies[0].version** and no **$.technologies[0].title** do not add a preface',
+    returns: 'String',
+    parameters: [
+      {
+        label: 'title',
+        type: 'String',
+        optional: false,
+        default: null,
+        documentation: 'The title string',
+      },
+      {
+        label: 'row',
+        type: 'Object',
+        optional: false,
+        default: null,
+        documentation: 'The current Percipio data we are processing. This is normally **$**.',
+      },
+    ],
+    signature: '(title,row)',
+  },
+  {
     label: 'truncate',
     description:
       'Truncates a string to a set length, optionally adding a suffix to indicate truncation.',
@@ -30,44 +76,6 @@ export default [
     signature: '(value, limit, terminator)',
   },
   {
-    label: 'shortenUuid',
-    description: 'Shorten the supplied UUID string, by converting to base64 string',
-    returns: 'String',
-    parameters: [
-      {
-        label: 'uuid',
-        type: 'String',
-        optional: false,
-        default: null,
-        documentation: 'The uuid string to shorten',
-      },
-    ],
-    signature: '(uuid)',
-  },
-  {
-    label: 'extendedTitle',
-    description:
-      'Get the extended title.\n\n  * If title already includes a colon no nothing\n  * If we have a **$.technologies**, preface the title with the **$.technologies[0].title and $.technologies[0].version**\n  * If we have a **$.technologies[0].title** and no **$.technologies[0].version** only preface with **$.technologies[0].title**\n  * If we have a **$.technologies[0].version** and no **$.technologies[0].title** do not add a preface',
-    returns: 'String',
-    parameters: [
-      {
-        label: 'title',
-        type: 'String',
-        optional: false,
-        default: null,
-        documentation: 'The title string',
-      },
-      {
-        label: 'row',
-        type: 'Object',
-        optional: false,
-        default: null,
-        documentation: 'The current Percipio data we are processing. This is normally **$**.',
-      },
-    ],
-    signature: '(title,row)',
-  },
-  {
     label: 'getContentType',
     description:
       'Normalise the content type.\n\n  * If **$.contentType.displayLabel** is *Book Summary* or *Book Review* return **BOOK SUMMARY**\n  * If **$.contentType.displayLabel** is *Audiobook Summary* return **AUDIO SUMMARY**\n  * If **$.contentType.displayLabel** is *LINKED_CONTENT* return **LINKED_CONTENT**\n  * For all others return **$.contentType.percipioType**\n',
@@ -82,6 +90,22 @@ export default [
       },
     ],
     signature: '(row)',
+  },
+  {
+    label: 'localeLanguage',
+    description:
+      'Get the name of the language. For example, en-US would return "English". fr-CA would return "French"',
+    returns: 'String',
+    parameters: [
+      {
+        label: 'localeCode',
+        type: 'String',
+        optional: false,
+        default: null,
+        documentation: 'The Percipio localeCode value to look up.',
+      },
+    ],
+    signature: '(localeCode)',
   },
   {
     label: 'isoDurationToHours',
@@ -148,6 +172,21 @@ export default [
     signature: '(duration)',
   },
   {
+    label: 'isoDurationToHHMMSSFormat',
+    description: 'Convert an ISO8601 formatted duration to HH:MM:SS formatted string',
+    returns: 'String',
+    parameters: [
+      {
+        label: 'duration',
+        type: 'String',
+        optional: false,
+        default: null,
+        documentation: 'An ISO8601 formatted duration string.',
+      },
+    ],
+    signature: '(duration)',
+  },
+  {
     label: 'stripQueryStringandHash',
     description: 'Remove any hash or querystring values for a URL',
     returns: 'String',
@@ -162,22 +201,7 @@ export default [
     ],
     signature: '(url)',
   },
-  {
-    label: 'localeLanguage',
-    description:
-      'Get the name of the language. For example, en-US would return "English". fr-CA would return "French"',
-    returns: 'String',
-    parameters: [
-      {
-        label: 'localeCode',
-        type: 'String',
-        optional: false,
-        default: null,
-        documentation: 'The Percipio localeCode value to look up.',
-      },
-    ],
-    signature: '(localeCode)',
-  },
+
   {
     label: 'plainString',
     description:
@@ -201,6 +225,45 @@ export default [
       },
     ],
     signature: '(value, limit)',
+  },
+  {
+    label: 'formatTitle',
+    description:
+      'The title that has conditionally had the lmsType and/or technology and technology version added to the beginning and/or end as configured, and truncated to a set length adding a "..." suffix to indicate truncation.',
+    returns: 'String',
+    parameters: [
+      {
+        label: 'titleFormat',
+        type: 'Object',
+        optional: false,
+        default: null,
+        documentation:
+          'An object with two attributes, **type** and **technology**\n\n * *type* can be: NONE, PREFIX, or POSTFIX\n\n * *technology* can be: NONE, PREFIX, or POSTFIX',
+      },
+      {
+        label: 'lmsType',
+        type: 'String',
+        optional: false,
+        default: null,
+        documentation: 'The string that represents the display label for the LMS',
+      },
+      {
+        label: 'limit',
+        type: 'number',
+        optional: false,
+        default: null,
+        documentation:
+          'The maximum number of characters including the "..." indicator that the string as been truncated',
+      },
+      {
+        label: 'row',
+        type: 'Object',
+        optional: false,
+        default: null,
+        documentation: 'The current Percipio data we are processing. This is normally **$**.',
+      },
+    ],
+    signature: '(titleFormat, lmsType, limit, row)',
   },
   {
     label: 'metadataExtendedDescription',
@@ -302,45 +365,7 @@ export default [
     ],
     signature: '(types, row)',
   },
-  {
-    label: 'formatTitle',
-    description:
-      'The title that has conditionally had the lmsType and/or technology and technology version added to the beginning and/or end as configured, and truncated to a set length adding a "..." suffix to indicate truncation.',
-    returns: 'String',
-    parameters: [
-      {
-        label: 'titleFormat',
-        type: 'Object',
-        optional: false,
-        default: null,
-        documentation:
-          'An object with two attributes, **type** and **technology**\n\n * *type* can be: NONE, PREFIX, or POSTFIX\n\n * *technology* can be: NONE, PREFIX, or POSTFIX',
-      },
-      {
-        label: 'lmsType',
-        type: 'String',
-        optional: false,
-        default: null,
-        documentation: 'The string that represents the display label for the LMS',
-      },
-      {
-        label: 'limit',
-        type: 'number',
-        optional: false,
-        default: null,
-        documentation:
-          'The maximum number of characters including the "..." indicator that the string as been truncated',
-      },
-      {
-        label: 'row',
-        type: 'Object',
-        optional: false,
-        default: null,
-        documentation: 'The current Percipio data we are processing. This is normally **$**.',
-      },
-    ],
-    signature: '(titleFormat, lmsType, limit, row)',
-  },
+
   {
     label: 'formatSummary',
     description:
